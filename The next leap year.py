18660a2c9143ec6485a966d0196d8850f9aeed32
Ahmed mo <ahmedmo501296@gmail.com>
2022-02-28 21:05:26 +0000

    First commit

---
+++ The next leap year.py
@@ -0,0 +1,18 @@
+
+i = 0
+year = int(input("Year:"))
+
+y = year
+
+  
+while True:
+    y = y + 1 
+    
+    if y % 4 == 0 and y % 100 != 0 and y % 400 != 0:
+        break
+    
+print(f"The next leap year after {year} is {y}") 
+ 
+     
+
+      
