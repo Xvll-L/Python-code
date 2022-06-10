5e52193181d9eee154c715f25f84750a49ab9d60
Ahmed mo <ahmedmo501296@gmail.com>
2022-06-10 12:49:19 +0100

    last commit test | added test to explaning programme | next test is git push

--- The next leap year.py
+++ The next leap year.py
@@ -1,16 +1,20 @@
 #leap year python git test
+
+# the viribale i is used to store data and update after each while loop
 i = 0
+#this will ask user input then convert the number to string
 year = int(input("Year:"))
 
+#used to store the year and add 1
 y = year
 
-  
+#while test if it leap year
 while True:
     y = y + 1 
     
     if y % 4 == 0 and y % 100 != 0 and y % 400 != 0:
         break
-    
+#print out results   
 print(f"The next leap year after {year} is {y}") 
  
      
